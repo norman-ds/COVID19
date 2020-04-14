092dc7023ff95c10d06987ed89d9340540fb0d73
Norman <normandev@puntaminar.ch>
2020-04-14 15:01:47 +0200

    initial commit

---
+++ R/model_sir.R
@@ -0,0 +1,61 @@
+#' SIR model
+#'
+#' These function returns the delta of the classic [SIR model](https://www.idmod.org/docs/hiv/model-sir.html#sir-model) without births or deaths.
+###' @param t vector of time steps
+#' @param y vector of model variables S,I,R.
+#' @param parms vector of model parameters R0, DoI, N.
+#' @return equation list
+
+SIR <- function(t, y, parms) {
+  
+  if (!all(c("R0", "DoI", "N") %in% names(parms))) {
+    stop("The SIR model requires R0 (recoverd population at time zero), DoI (duration, D, of infection) and N (total population).")
+  }
+  
+  # constant rates
+  gamma <- 1/parms["DoI"]
+  beta <- parms["R0"] * gamma / parms["N"]
+  
+  # Delta Susceptibles S
+  dS <- - beta * y["S"] * y["I"]
+  
+  # Delta Infecteds I
+  dI <- beta * y["S"] * y["I"] -
+    gamma * y["I"]
+  
+  # Delta Recovereds R
+  dR <- gamma * y["I"]
+  
+  dy <- c(dS, dI, dR)
+  names(dy) <- c("dS","dI","dR")
+  return(list(dy))
+  
+}
+
+
+# initialization 
+t <- seq(0, 180, by=1) # time points
+DoI <- 20 # average duration of infection
+N <- 8.57e6 # population Switzerland
+I0 <- 100 # infectious population at time zero
+R0 <- 10 # recoverd population at time zero
+S0 <- N-I0-R0 # susceptible population at time zero
+
+y0 <- c(S=S0, I=I0, R=R0)
+parms <- c(R0=R0, N=N, DoI=DoI)
+(dy <- SIR(t, y0, parms))
+
+y.names <- c("susceptible ", "infectious", "recoverd")
+y.mat <- deSolve::lsoda(y = y0,
+                       times = t,
+                       func = SIR,
+                       parms = parms)
+
+layout(matrix(1:2, nrow=1))
+matplot(t, y.mat[,2:4], type = "l", ylab = "N", col=1)
+legend("right", y.names , lty=5:1, col=1, bty="n", title = "population")
+matplot(t, y.mat[,2:4], type = "l", ylab = "N", log = "y", col=1)
+
+##df <- solve_eqns(SIR, y0, t, constants)
+
+
--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# COVID19
-SIR model as Shiny App
+---
+title: "Can we afford to have too little data?"
+author: "Norman Bieri"
+date: "`r paste(Sys.Date())`"
+output: html_document
+---
+
+```{r setup, include=FALSE}
+knitr::opts_chunk$set(echo = TRUE)
+```
+
+
+We know little about the speed at which viruses are prepared in our society. The technology is available, but the implementation leaves large gaps. How many more gaps can we afford ? An overview of the spread of COVID-19 in Switzerland and a possible mutation should indicate the extent.
+
+# How to create a reproduceable Shiny-App
+
+Start [RStudio](https://rstudio.com) in a [docker](https://www.docker.com), which always guarantees you the same ecos-system [rocker/rstudio:3.6.3](https://github.com/rocker-org/rocker). Link a local volume (the current working directory, $(pwd)) to the rstudio container. Running the following command will start RStudio in http://localhost:8787:
+
+**package deSolve and tidyr   !!!**
+
+
+``` ba
+docker run -d -p 8787:8787 -v $(pwd):/home/rstudio -e PASSWORD=mypwd rocker/rstudio:3.6.3
+```
+
+# Models
+
+[SIR Model](https://www.idmod.org/docs/hiv/model-sir.html#sir-model)
+A generic epidemiological model that provides a simplified means of describing the transmission of an infectious disease through individuals where those individuals can pass through the following five states: susceptible, infectious, and recovered.
